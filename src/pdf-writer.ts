1b99052dce422d93f0206a235f1b2f9e6181c8d8
LucSchimidt <llourencoschimidt@gmail.com>
2024-12-28 15:21:11 -0300

    final_changes

--- src/pdf-writer.ts
+++ src/pdf-writer.ts
@@ -48,6 +48,22 @@ interface RegisterDetails {
     packages: Package[];
 }
 
+interface AboutPage {
+  aboutRealDoc: string;
+  registrationEnvironment: string;
+  registeredContentTypes: string;
+  websiteTechnicalCapture: string;
+  fileIntegrityWithHashCodes: string;
+  pdfAReport: string;
+  digitalSignatureAndTimestampICP: string;
+  complementaryServices: string;
+  registrationScopeAndObject: string;
+  technicalSecurityAndCompliance: string;
+  registrationValidation: string;
+  realdocValidator: string;
+  manualValidation: string;
+  essentialLegalAspects: string;
+}
 
 class GenerateReport {
   private doc: PDFKit.PDFDocument;
@@ -385,6 +401,149 @@ class GenerateReport {
       this.doc.image('./assets/logo.png', xPosition, yPosition, { fit: [125, 125], align: 'center', valign:'bottom'});
     }
 
+    //Gera as páginas sobre a empresa:
+    aboutPage(about_content:AboutPage) {
+      this.doc.addPage({layout:'portrait'})
+
+      let start_y = 100
+      this.doc.fillColor('black');
+      this.doc.font('Helvetica-Bold')
+      this.doc.fontSize(16).text('Sobre a REALDOC:', 75, start_y);
+
+      this.doc.font('Helvetica')
+      this.doc.fillColor('#4B5563');
+      this.doc.fontSize(12).text(`${about_content.aboutRealDoc}`);
+
+      this.doc.moveDown()
+
+      this.doc.fillColor('black');
+      this.doc.font('Helvetica-Bold')
+      this.doc.fontSize(12).text('O ambiente de registro:');
+      this.doc.font('Helvetica')
+      this.doc.fillColor('#4B5563');
+      this.doc.fontSize(12).text(`${about_content.registrationEnvironment}`);
+
+      this.doc.moveDown()
+
+      this.doc.fillColor('black');
+      this.doc.font('Helvetica-Bold')
+      this.doc.fontSize(12).text('Tipos de conteúdos registrados:');
+      this.doc.font('Helvetica')
+      this.doc.fillColor('#4B5563');
+      this.doc.fontSize(12).text(`${about_content.registeredContentTypes}`);
+
+      this.doc.moveDown()
+
+      this.doc.fillColor('black');
+      this.doc.font('Helvetica-Bold')
+      this.doc.fontSize(12).text('Captura técnica de websites:');
+      this.doc.font('Helvetica')
+      this.doc.fillColor('#4B5563');
+      this.doc.fontSize(12).text(`${about_content.websiteTechnicalCapture}`);
+
+      this.doc.moveDown()
+
+      this.doc.fillColor('black');
+      this.doc.font('Helvetica-Bold')
+      this.doc.fontSize(12).text('Integridade dos arquivos através de códigos HASH:');
+      this.doc.font('Helvetica')
+      this.doc.fillColor('#4B5563');
+      this.doc.fontSize(12).text(`${about_content.fileIntegrityWithHashCodes}`);
+
+      this.doc.moveDown()
+
+      this.doc.fillColor('black');
+      this.doc.font('Helvetica-Bold')
+      this.doc.fontSize(12).text('Relatório PDF/A:');
+      this.doc.font('Helvetica')
+      this.doc.fillColor('#4B5563');
+      this.doc.fontSize(12).text(`${about_content.pdfAReport}`);
+
+      this.doc.moveDown()
+
+      this.doc.fillColor('black');
+      this.doc.font('Helvetica-Bold')
+      this.doc.fontSize(12).text('Assinatura Digital e carimbo de tempo ICP/Brasil:');
+      this.doc.font('Helvetica')
+      this.doc.fillColor('#4B5563');
+      this.doc.fontSize(12).text(`${about_content.digitalSignatureAndTimestampICP}`);
+
+      this.doc.moveDown()
+
+      this.doc.fillColor('black');
+      this.doc.font('Helvetica-Bold')
+      this.doc.fontSize(12).text('Serviços complementares:');
+      this.doc.font('Helvetica')
+      this.doc.fillColor('#4B5563');
+      this.doc.fontSize(12).text(`${about_content.complementaryServices}`);
+
+      this.doc.moveDown()
+
+      this.doc.fillColor('black');
+      this.doc.font('Helvetica-Bold')
+      this.doc.fontSize(12).text('Escopo e objeto de registro:');
+      this.doc.font('Helvetica')
+      this.doc.fillColor('#4B5563');
+      this.doc.fontSize(12).text(`${about_content.registrationScopeAndObject}`);
+
+      this.doc.moveDown()
+
+      this.doc.fillColor('black');
+      this.doc.font('Helvetica-Bold')
+      this.doc.fontSize(12).text('Segurança e conformidade de técnica:');
+      this.doc.font('Helvetica')
+      this.doc.fillColor('#4B5563');
+      this.doc.fontSize(12).text(`${about_content.technicalSecurityAndCompliance}`);
+
+      this.doc.moveDown()
+
+      this.doc.fillColor('black');
+      this.doc.font('Helvetica-Bold')
+      this.doc.fontSize(16).text('Validação do Registro:');
+
+      this.doc.font('Helvetica')
+      this.doc.fillColor('#4B5563');
+      this.doc.fontSize(12).text(`${about_content.registrationValidation}`);
+
+      this.doc.moveDown()
+
+      this.doc.fillColor('black');
+      this.doc.font('Helvetica-Bold')
+      this.doc.fontSize(12).text('Validador REALDOC');
+      this.doc.font('Helvetica')
+      this.doc.fillColor('#4B5563');
+      this.doc.fontSize(12).text(`${about_content.realdocValidator}`);
+
+      this.doc.moveDown()
+
+      this.doc.fillColor('black');
+      this.doc.font('Helvetica-Bold')
+      this.doc.fontSize(12).text('Validação manual:');
+      this.doc.font('Helvetica')
+      this.doc.fillColor('#4B5563');
+      this.doc.fontSize(12).text(`${about_content.manualValidation}`);
+
+      this.doc.moveDown()
+
+      this.doc.fillColor('black');
+      this.doc.font('Helvetica-Bold')
+      this.doc.fontSize(16).text('Aspectos jurídicos essenciais:');
+
+      this.doc.font('Helvetica')
+      this.doc.fillColor('#4B5563');
+      this.doc.fontSize(12).text(`${about_content.essentialLegalAspects}`);
+
+      this.doc.moveDown();
+
+      const imageWidth = 125;
+      const pageWidth = this.doc.page.width;
+      const xPosition = (pageWidth - imageWidth) / 2;
+      const imageHeight = 125;
+      const pageHeight = this.doc.page.height;
+      const yPosition = pageHeight - imageHeight - this.doc.page.margins.bottom + 50;
+      this.doc.image('./assets/logo.png', xPosition, yPosition, { fit: [125, 125], align: 'center', valign:'bottom'});
+    }
+
     //!Método essencial para gerar o arquivo PDF.
     generate() {
       this.doc.end();
@@ -395,30 +554,52 @@ class GenerateReport {
 
 //EXEMPLO DE EXECUÇÃO DO PDF:
 
-const meu_relatorio = new GenerateReport('./relatorio.pdf', '090503', 'Relatório de organização dos periféricos', 'Lucas L. Schimidt');
+function geraRelatorio() {
+  const meu_relatorio = new GenerateReport('./relatorio.pdf', '090503', 'Relatório de organização dos periféricos', 'Lucas L. Schimidt');
 
-const introduction_text:string = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tincidunt arcu at ligula ullamcorper, eget egestas dui ullamcorper. Integer vulputate sapien at bibendum vehicula. Nulla facilisi. Mauris egestas, ipsum ac interdum iaculis, neque lorem vulputate arcu, ac consequat elit justo sit amet purus. Curabitur vehicula auctor ligula, eu viverra odio posuere ac. Curabitur id lorem in libero placerat scelerisque non sit amet neque. Morbi interdum dui eu mauris aliquam, at aliquet justo sodales. Quisque faucibus tempor turpis, sit amet egestas orci vestibulum ac. Vivamus fermentum libero et sem condimentum, sed rhoncus velit vestibulum. Integer feugiat sollicitudin ligula, et tincidunt ligula placerat et. Donec eget est nec sapien euismod convallis id sed augue. Nullam varius purus eget dui gravida, non rutrum purus facilisis. Vivamus ut ligula hendrerit, aliquam dui non, auctor orci. Sed consectetur justo quis bibendum convallis. Aenean eget erat venenatis, euismod purus vitae, tincidunt orci. Ut feugiat, orci nec tincidunt feugiat, nulla dolor convallis velit, sed vehicula est enim a ligula. Aenean auctor ipsum sapien, eu euismod felis auctor nec. Sed fermentum, magna vitae fermentum pretium, enim felis laoreet enim, sit amet mollis purus felis ac purus. Quisque pretium, lectus a laoreet finibus, urna lorem efficitur nulla, euismod dictum purus sapien sed est. Sed tempor massa nisi, at convallis elit sodales at. Aliquam a felis vitae dolor dapibus tempus. Donec fermentum bibendum massa, ac malesuada lacus viverra ut. Etiam cursus orci in nulla rutrum, ac fringilla risus tincidunt. Nunc tincidunt orci sed arcu tincidunt facilisis. In eget lectus felis. Nam in nunc dolor. Ut suscipit, lorem eu euismod volutpat, mi erat tincidunt sem, eget placerat magna augue eu erat. Fusce aliquet, sem non lobortis efficitur, ante dui elementum tortor, ac luctus purus felis sit amet arcu. Donec et dui sed odio maximus gravida et a sapien. Vestibulum ac magna arcu. Aliquam erat volutpat. Nullam dignissim, nisi vel facilisis gravida, dui nulla efficitur nulla, sed sollicitudin nulla ante et dui. Donec tristique libero sed sapien lobortis, et iaculis neque finibus. Aliquam erat volutpat. Cras feugiat viverra risus, ac tincidunt sapien aliquet eu. Mauris vel nisi vitae eros iaculis lacinia. Mauris rutrum urna risus, id tempus eros efficitur ut. Phasellus congue vestibulum ante et consectetur. Fusce efficitur auctor eros id pharetra. Ut vehicula suscipit mauris ac tempor. Aliquam erat volutpat. Nam dapibus cursus urna, eu vestibulum orci tempor sed.'
-meu_relatorio.introductionPage(introduction_text)
-meu_relatorio.certificationPage('./assets/qr_code.png','193j1e98u12eh1982u3912dh1', 'validation.com')
+  const introduction_text:string = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tincidunt arcu at ligula ullamcorper, eget egestas dui ullamcorper. Integer vulputate sapien at bibendum vehicula. Nulla facilisi. Mauris egestas, ipsum ac interdum iaculis, neque lorem vulputate arcu, ac consequat elit justo sit amet purus. Curabitur vehicula auctor ligula, eu viverra odio posuere ac. Curabitur id lorem in libero placerat scelerisque non sit amet neque. Morbi interdum dui eu mauris aliquam, at aliquet justo sodales. Quisque faucibus tempor turpis, sit amet egestas orci vestibulum ac. Vivamus fermentum libero et sem condimentum, sed rhoncus velit vestibulum. Integer feugiat sollicitudin ligula, et tincidunt ligula placerat et. Donec eget est nec sapien euismod convallis id sed augue. Nullam varius purus eget dui gravida, non rutrum purus facilisis. Vivamus ut ligula hendrerit, aliquam dui non, auctor orci. Sed consectetur justo quis bibendum convallis. Aenean eget erat venenatis, euismod purus vitae, tincidunt orci. Ut feugiat, orci nec tincidunt feugiat, nulla dolor convallis velit, sed vehicula est enim a ligula. Aenean auctor ipsum sapien, eu euismod felis auctor nec. Sed fermentum, magna vitae fermentum pretium, enim felis laoreet enim, sit amet mollis purus felis ac purus. Quisque pretium, lectus a laoreet finibus, urna lorem efficitur nulla, euismod dictum purus sapien sed est. Sed tempor massa nisi, at convallis elit sodales at. Aliquam a felis vitae dolor dapibus tempus. Donec fermentum bibendum massa, ac malesuada lacus viverra ut. Etiam cursus orci in nulla rutrum, ac fringilla risus tincidunt. Nunc tincidunt orci sed arcu tincidunt facilisis. In eget lectus felis. Nam in nunc dolor. Ut suscipit, lorem eu euismod volutpat, mi erat tincidunt sem, eget placerat magna augue eu erat. Fusce aliquet, sem non lobortis efficitur, ante dui elementum tortor, ac luctus purus felis sit amet arcu. Donec et dui sed odio maximus gravida et a sapien. Vestibulum ac magna arcu. Aliquam erat volutpat. Nullam dignissim, nisi vel facilisis gravida, dui nulla efficitur nulla, sed sollicitudin nulla ante et dui. Donec tristique libero sed sapien lobortis, et iaculis neque finibus. Aliquam erat volutpat. Cras feugiat viverra risus, ac tincidunt sapien aliquet eu. Mauris vel nisi vitae eros iaculis lacinia. Mauris rutrum urna risus, id tempus eros efficitur ut. Phasellus congue vestibulum ante et consectetur. Fusce efficitur auctor eros id pharetra. Ut vehicula suscipit mauris ac tempor. Aliquam erat volutpat. Nam dapibus cursus urna, eu vestibulum orci tempor sed.'
+  meu_relatorio.introductionPage(introduction_text)
+  meu_relatorio.certificationPage('./assets/qr_code.png','193j1e98u12eh1982u3912dh1', 'validation.com')
 
-//Adicionando os dados da página de detalhes de registro:
-const pacote_um:Package = {archive_name: 'capture_6711396f6336070c.zip', size: 13.74, type: 'Conteúdos capturados', hash: ['09a8sdasnd12uh3jasd', 'asjsi45645uhuh9283190823']} 
-const detalhes_do_registro:RegisterDetails = {id: '90123j1209ujed182ji123', start_date: '2024-04-03', end_date:'2024-04-07', session_time:200, utf:'(UTC-03:00) Brasilia', ambiente:'WEBSITE - Ponto(s) de acesso à internet: 45.170.27.220', packages:[pacote_um]}
-meu_relatorio.registerDetails(detalhes_do_registro)
+  //Adicionando os dados da página de detalhes de registro:
+  const pacote_um:Package = {archive_name: 'capture_6711396f6336070c.zip', size: 13.74, type: 'Conteúdos capturados', hash: ['09a8sdasnd12uh3jasd', 'asjsi45645uhuh9283190823']} 
+  const detalhes_do_registro:RegisterDetails = {id: '90123j1209ujed182ji123', start_date: '2024-04-03', end_date:'2024-04-07', session_time:200, utf:'(UTC-03:00) Brasilia', ambiente:'WEBSITE - Ponto(s) de acesso à internet: 45.170.27.220', packages:[pacote_um]}
+  meu_relatorio.registerDetails(detalhes_do_registro)
 
 
-//Adicionando imagens:
-const imagem_um:Imagem = {archive_name: 'minha_imagem.png', size: 20.0,  path: './assets/captura.png', date: '2024-12-24', origin: 'google.com', origin_url: 'https://www.google.com', hash:['hash_exemplo_um11111', 'hash_exemplo_dois2222']}
-meu_relatorio.addImage(imagem_um)
+  //Adicionando imagens:
+  const imagem_um:Imagem = {archive_name: 'minha_imagem.png', size: 20.0,  path: './assets/captura.png', date: '2024-12-24', origin: 'google.com', origin_url: 'https://www.google.com', hash:['hash_exemplo_um11111', 'hash_exemplo_dois2222']}
+  meu_relatorio.addImage(imagem_um)
 
-const video_um:Video = {archive_name: 'meu_video.mp4', hash: '1928321hsd1hge129873ghdjhga', start_time:'00:00:00', end_time:'00:25:52', duration: 300}
-const video_dois:Video = {archive_name: 'meu_video_2.mp4', hash: '1928321hsdasd1hge129873ghdjhga', start_time:'00:00:00', end_time:'00:03:11', duration: 300}
+  const video_um:Video = {archive_name: 'meu_video.mp4', hash: '1928321hsd1hge129873ghdjhga', start_time:'00:00:00', end_time:'00:25:52', duration: 300}
+  const video_dois:Video = {archive_name: 'meu_video_2.mp4', hash: '1928321hsdasd1hge129873ghdjhga', start_time:'00:00:00', end_time:'00:03:11', duration: 300}
 
-const historico_um:Historico = {url: {link: 'https://google.com', created_at: '2008-05-10', ip_address:'192.168.0.0', owner:'Google'}, datetime: '2024-12-24 00:00:00'}
-const historico_dois:Historico = {url: {link: 'https://google.com', created_at: '2008-05-10', ip_address:'192.168.0.0', owner:'Google'}, datetime: '2024-12-24 00:00:00'}
-const historico_tres:Historico = {url: {link: 'https://google.com', created_at: '2008-05-10', ip_address:'192.168.0.0', owner:'Google'}, datetime: '2024-12-24 00:00:00'}
-const historico:Historico[] = [historico_um, historico_dois, historico_tres]
+  const historico_um:Historico = {url: {link: 'https://google.com', created_at: '2008-05-10', ip_address:'192.168.0.0', owner:'Google'}, datetime: '2024-12-24 00:00:00'}
+  const historico_dois:Historico = {url: {link: 'https://google.com', created_at: '2008-05-10', ip_address:'192.168.0.0', owner:'Google'}, datetime: '2024-12-24 00:00:00'}
+  const historico_tres:Historico = {url: {link: 'https://google.com', created_at: '2008-05-10', ip_address:'192.168.0.0', owner:'Google'}, datetime: '2024-12-24 00:00:00'}
+  const historico:Historico[] = [historico_um, historico_dois, historico_tres]
 
-meu_relatorio.videoAndHistoryDetails([video_um, video_dois], historico)
+  meu_relatorio.videoAndHistoryDetails([video_um, video_dois], historico)
 
-meu_relatorio.generate()
+  const strings: AboutPage = {
+    aboutRealDoc: "About REALDOC",
+    registrationEnvironment: "Registration Environment",
+    registeredContentTypes: "Types of Registered Content",
+    websiteTechnicalCapture: "Website Technical Capture",
+    fileIntegrityWithHashCodes: "File Integrity through HASH Codes",
+    pdfAReport: "PDF/A Report",
+    digitalSignatureAndTimestampICP: "Digital Signature and Timestamp ICP/Brazil",
+    complementaryServices: "Complementary Services",
+    registrationScopeAndObject: "Registration Scope and Object",
+    technicalSecurityAndCompliance: "Technical Security and Compliance",
+    registrationValidation: "Registration Validation",
+    realdocValidator: "Verifact Validator",
+    manualValidation: "Manual Validation",
+    essentialLegalAspects: "Essential Legal Aspects"
+  };
+  meu_relatorio.aboutPage(strings)
+
+  meu_relatorio.generate()
+}
+
+geraRelatorio()
